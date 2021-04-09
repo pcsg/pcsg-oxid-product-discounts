ee4fc9c2a8ac13a8f580cfa1cad68086e79ae31c
Jan Wennrich <jan.wennrich@pcsg.de>
2021-04-09 18:10:20 +0200

    style: improves code-style in module_options.php

--- Application/views/admin/de/module_options.php
+++ Application/views/admin/de/module_options.php
@@ -4,18 +4,18 @@ $sLangName = "Deutsch";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
-$aLang = array(
-    'charset'                         => 'UTF-8',
+$aLang = [
+    'charset'                                    => 'UTF-8',
 
     // Guests
-    'SHOP_MODULE_GROUP_guests'       => 'Gäste',
-    'SHOP_MODULE_discount_guest_group_id'      => 'Gast GruppenID',
-    'HELP_SHOP_MODULE_discount_guest_group_id' => 'Die ID der Gruppe, welche für Gäste verwendet werden soll.',
+    'SHOP_MODULE_GROUP_guests'                   => 'Gäste',
+    'SHOP_MODULE_discount_guest_group_id'        => 'Gast GruppenID',
+    'HELP_SHOP_MODULE_discount_guest_group_id'   => 'Die ID der Gruppe, welche für Gäste verwendet werden soll.',
 
     // groups
-    'SHOP_MODULE_GROUP_groups' => 'Gruppen-Einstellungen',
-    'SHOP_MODULE_non_costumer_groups' => 'Nicht-Kunden-Gruppen',
-    'HELP_SHOP_MODULE_non_costumer_groups' => 'Hier können kommasepariert die Ids von Gruppen denen nicht nach der Bestellung die Gruppe Kunden zugeordnet werden soll eingetragen werden.',
-    'SHOP_MODULE_custom_new_customer_group' => 'GruppenId für neue Kunden',
+    'SHOP_MODULE_GROUP_groups'                   => 'Gruppen-Einstellungen',
+    'SHOP_MODULE_non_costumer_groups'            => 'Nicht-Kunden-Gruppen',
+    'HELP_SHOP_MODULE_non_costumer_groups'       => 'Hier können kommasepariert die Ids von Gruppen denen nicht nach der Bestellung die Gruppe Kunden zugeordnet werden soll eingetragen werden.',
+    'SHOP_MODULE_custom_new_customer_group'      => 'GruppenId für neue Kunden',
     'HELP_SHOP_MODULE_custom_new_customer_group' => 'Hier kann eine GruppenId hinterlegt werden der fortan alle Kunden bei Neubestellung hinzugefügt werden.'
-);
+];
